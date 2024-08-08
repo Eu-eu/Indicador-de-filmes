62fe32f4a8c37f1ea2198be432327671bf52b56f
Camila <52725649+Eu-eu@users.noreply.github.com>
2024-08-08 09:38:37 -0300

    adicionando css e js

---
+++ sketch.js
@@ -0,0 +1,21 @@
+} else {
+    return "Ladrões de bicicleta"; 
+    }
+  } else {
+    if(gostaDeFantasia){
+        return "As aventuras de Pi";
+    } else {
+        return "Depois da chuva";
+    }
+}
+}
+} else {
+if(gostaDeFantasia) {
+    return "A viagem de Chihiro";
+} else {
+    return "O feitiço do tempo";
+}
+}
+}
+
+
